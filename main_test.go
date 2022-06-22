b71789fddb56c5f95ca284b04a0e497f0d32057b
Jonas <jojo@srg.com>
2022-06-22 10:11:37 +0200

    8min11s second green test

--- main_test.go
+++ main_test.go
@@ -6,6 +6,9 @@ import (
 )
 
 func beat(prop1 string, prop2 string) bool {
+	if prop2 == "paper" {
+		return false
+	}
 	return true
 }
 
@@ -14,4 +17,11 @@ func TestRockBeatScissors(t *testing.T) {
 	prop2 := "scissors"
 	res := beat(prop1, prop2)
 	assert.True(t, res, "Rock should beat scissors.")
+}
+
+func TestRockLoseAgainstPaper(t *testing.T) {
+	prop1 := "rock"
+	prop2 := "paper"
+	res := beat(prop1, prop2)
+	assert.False(t, res, "Rock should lose against paper.")
 }
