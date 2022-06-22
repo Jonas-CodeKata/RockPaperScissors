1f408c58d0fca88064bf4c3e5ec0fd8a74bf59ee
Jonas <jojo@srg.com>
2022-06-22 10:08:23 +0200

    5min41s first green test

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func beat(prop1 string, prop2 string) bool {
+	return true
+}
+
+func TestRockBeatScissors(t *testing.T) {
+	prop1 := "rock"
+	prop2 := "scissors"
+	res := beat(prop1, prop2)
+	assert.True(t, res, "Rock should beat scissors.")
+}
