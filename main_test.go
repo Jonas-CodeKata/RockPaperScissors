7f966ba416da76cfbb0dc1de53cddbbe0b37d6a9
Jonas <jojo@srg.com>
2022-06-22 10:24:42 +0200

    19min26s 3 green tests after refacto using const with iota.

--- main_test.go
+++ main_test.go
@@ -5,40 +5,50 @@ import (
 	"testing"
 )
 
+type result = int
+
 const (
-	draw = iota
+	draw result = iota
 	win
 	lose
 )
 
-func beat(prop1 string, prop2 string) int {
+type shape = int
+
+const (
+	rock shape = iota
+	paper
+	scissors
+)
+
+func beat(prop1 shape, prop2 shape) result {
 	if prop1 == prop2 {
 		return draw
 	}
 
-	if prop2 == "paper" {
+	if prop2 == paper {
 		return lose
 	}
 	return win
 }
 
 func TestRockBeatScissors(t *testing.T) {
-	prop1 := "rock"
-	prop2 := "scissors"
+	prop1 := rock
+	prop2 := scissors
 	res := beat(prop1, prop2)
 	assert.Equal(t, win, res, "Rock should beat scissors.")
 }
 
 func TestRockLoseAgainstPaper(t *testing.T) {
-	prop1 := "rock"
-	prop2 := "paper"
+	prop1 := rock
+	prop2 := paper
 	res := beat(prop1, prop2)
 	assert.Equal(t, lose, res, "Rock should lose against paper.")
 }
 
 func TestRockDrawAgainstItSelf(t *testing.T) {
-	prop1 := "rock"
-	prop2 := "rock"
+	prop1 := rock
+	prop2 := rock
 	res := beat(prop1, prop2)
 	assert.Equal(t, draw, res, "Rock vs Rock should be draw.")
 }
